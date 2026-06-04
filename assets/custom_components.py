5a60bed232502fcb48c96aa6905cbb8c78011e76
meghashreemb-tech <meghashree.mb@vidyavikas.edu.in>
2026-06-04 12:33:24 +0530

    Update custom_components.py

--- assets/custom_components.py
+++ assets/custom_components.py
@@ -1,1 +1,21 @@
+import streamlit as st
 
+def page_header(title, subtitle):
+
+    st.markdown(
+        f"""
+        <div style="
+            background:linear-gradient(
+            90deg,
+            #0A66C2,
+            #4CAF50
+            );
+            padding:20px;
+            border-radius:15px;
+            color:white;">
+            <h1>{title}</h1>
+            <p>{subtitle}</p>
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
