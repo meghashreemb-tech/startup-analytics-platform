4b6489ec7a71b2751f05dc6eb0dbaff2bab8eaaa
meghashreemb-tech <meghashree.mb@vidyavikas.edu.in>
2026-06-04 12:48:39 +0530

    Update metrics.py

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,1 +1,19 @@
+def get_metrics(df):
 
+    metrics = {
+        "total_startups": len(df),
+
+        "total_funding":
+        df["Funding Amount (M USD)"].sum(),
+
+        "total_valuation":
+        df["Valuation (M USD)"].sum(),
+
+        "total_revenue":
+        df["Revenue (M USD)"].sum(),
+
+        "avg_employees":
+        round(df["Employees"].mean(), 2)
+    }
+
+    return metrics
