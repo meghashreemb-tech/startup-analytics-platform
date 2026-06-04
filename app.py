576cb9c8899534c68805267f8860642b73819029
meghashreemb-tech <meghashree.mb@vidyavikas.edu.in>
2026-06-04 12:38:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
+import streamlit as st
 
+from assets.load_css import local_css
+from assets.custom_components import page_header
+
+st.set_page_config(
+    page_title="Startup Analytics",
+    layout="wide"
+)
+
+local_css("assets/styles.css")
+
+page_header(
+    "🚀 Startup Analytics Platform",
+    "Deep Business Intelligence Dashboard"
+)
+
+st.image(
+    "assets/banner.jpg",
+    use_container_width=True
+)
