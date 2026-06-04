cd495e4fa8e605efed3e7a5c5b730168cdd23838
meghashreemb-tech <meghashree.mb@vidyavikas.edu.in>
2026-06-04 12:53:02 +0530

    Update validation.py

--- utils/validation.py
+++ utils/validation.py
@@ -1,1 +1,18 @@
+def validate_dataframe(df):
 
+    required_columns = [
+        "Startup Name",
+        "Industry",
+        "Funding Amount (M USD)",
+        "Valuation (M USD)",
+        "Revenue (M USD)"
+    ]
+
+    missing = []
+
+    for col in required_columns:
+
+        if col not in df.columns:
+            missing.append(col)
+
+    return missing
