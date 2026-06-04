733c5f8640f0db2eac798bdceb94679670bbd3f1
meghashreemb-tech <meghashree.mb@vidyavikas.edu.in>
2026-06-04 12:22:52 +0530

    Update Report_Generator.py

--- pages/Report_Generator.py
+++ pages/Report_Generator.py
@@ -1,1 +1,21 @@
+import streamlit as st
+import pandas as pd
 
+df = pd.read_csv("data/startup_data.csv")
+
+st.title("📄 Report Generator")
+
+st.subheader("Dataset Preview")
+
+st.dataframe(df.head())
+
+csv = df.to_csv(index=False)
+
+st.download_button(
+    label="Download CSV Report",
+    data=csv,
+    file_name="startup_report.csv",
+    mime="text/csv"
+)
+
+st.success("Report Ready")
