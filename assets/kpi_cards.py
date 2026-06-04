f0bec82c646d67735b0541ccdd0ac0ce31c34142
meghashreemb-tech <meghashree.mb@vidyavikas.edu.in>
2026-06-04 12:32:27 +0530

    Update kpi_cards.py

--- assets/kpi_cards.py
+++ assets/kpi_cards.py
@@ -1,1 +1,18 @@
+import streamlit as st
 
+def show_kpi(title, value):
+
+    st.markdown(
+        f"""
+        <div style="
+            background:white;
+            padding:20px;
+            border-radius:15px;
+            box-shadow:0px 4px 10px rgba(0,0,0,0.1);
+            text-align:center;">
+            <h4>{title}</h4>
+            <h2>{value}</h2>
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
