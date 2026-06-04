19adf311e74a57a8744a84e6cddf7651df2e53a6
meghashreemb-tech <meghashree.mb@vidyavikas.edu.in>
2026-06-04 12:19:45 +0530

    Update Startup_Explorer.py

--- pages/Startup_Explorer.py
+++ pages/Startup_Explorer.py
@@ -1,1 +1,19 @@
+import streamlit as st
+import pandas as pd
 
+df = pd.read_csv("data/startup_data.csv")
+
+st.title("🚀 Startup Explorer")
+
+industry = st.selectbox(
+    "Select Industry",
+    sorted(df["Industry"].unique())
+)
+
+filtered = df[df["Industry"]==industry]
+
+st.dataframe(filtered)
+
+st.write(
+    f"Total Startups: {len(filtered)}"
+)
