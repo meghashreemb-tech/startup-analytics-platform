ac98cc0d2b1c9e1f4d34d9dcff4c55b0797fe171
meghashreemb-tech <meghashree.mb@vidyavikas.edu.in>
2026-06-04 12:49:22 +0530

    Update filters.py

--- utils/filters.py
+++ utils/filters.py
@@ -1,1 +1,12 @@
+def filter_by_industry(df, industry):
 
+    return df[
+        df["Industry"] == industry
+    ]
+
+
+def filter_by_region(df, region):
+
+    return df[
+        df["Region"] == region
+    ]
