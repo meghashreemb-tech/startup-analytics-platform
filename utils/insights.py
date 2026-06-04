4a8b77edfb72738a02773175325b173e1f855ce3
meghashreemb-tech <meghashree.mb@vidyavikas.edu.in>
2026-06-04 12:50:18 +0530

    Update insights.py

--- utils/insights.py
+++ utils/insights.py
@@ -1,1 +1,37 @@
+def generate_insights(df):
 
+    insights = []
+
+    top_industry = (
+        df.groupby("Industry")
+        ["Valuation (M USD)"]
+        .sum()
+        .idxmax()
+    )
+
+    insights.append(
+        f"Top industry by valuation: {top_industry}"
+    )
+
+    top_region = (
+        df.groupby("Region")
+        ["Revenue (M USD)"]
+        .sum()
+        .idxmax()
+    )
+
+    insights.append(
+        f"Top revenue region: {top_region}"
+    )
+
+    profitable = (
+        df["Profitable"]
+        .value_counts()
+        .idxmax()
+    )
+
+    insights.append(
+        f"Most startups are profitable: {profitable}"
+    )
+
+    return insights
