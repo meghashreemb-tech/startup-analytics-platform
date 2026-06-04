4aef826d263b684b32b9a5403ca23be07e35e84b
meghashreemb-tech <meghashree.mb@vidyavikas.edu.in>
2026-06-04 12:20:47 +0530

    Update Deep_Analytics.py

--- pages/Deep_Analytics.py
+++ pages/Deep_Analytics.py
@@ -1,1 +1,59 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
 
+df = pd.read_csv("data/startup_data.csv")
+
+st.title("📈 Deep Analytics")
+
+tab1,tab2,tab3 = st.tabs([
+    "Valuation",
+    "Revenue",
+    "Market Share"
+])
+
+with tab1:
+
+    fig = px.scatter(
+        df,
+        x="Revenue (M USD)",
+        y="Valuation (M USD)",
+        color="Industry",
+        size="Employees",
+        hover_name="Startup Name"
+    )
+
+    st.plotly_chart(
+        fig,
+        use_container_width=True
+    )
+
+with tab2:
+
+    revenue = df.groupby(
+        "Industry"
+    )["Revenue (M USD)"].sum()
+
+    fig = px.bar(
+        revenue,
+        title="Revenue by Industry"
+    )
+
+    st.plotly_chart(
+        fig,
+        use_container_width=True
+    )
+
+with tab3:
+
+    fig = px.box(
+        df,
+        x="Industry",
+        y="Market Share (%)",
+        color="Industry"
+    )
+
+    st.plotly_chart(
+        fig,
+        use_container_width=True
+    )
