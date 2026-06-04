a91b0092b89f16765aa223b8b6835c69d7797e22
meghashreemb-tech <meghashree.mb@vidyavikas.edu.in>
2026-06-04 12:35:26 +0530

    Update charts.py

--- assets/charts.py
+++ assets/charts.py
@@ -1,1 +1,14 @@
+import plotly.express as px
 
+def valuation_chart(df):
+
+    fig = px.scatter(
+        df,
+        x="Revenue (M USD)",
+        y="Valuation (M USD)",
+        color="Industry",
+        size="Employees",
+        hover_name="Startup Name"
+    )
+
+    return fig
