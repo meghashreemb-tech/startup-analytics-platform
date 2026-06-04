89359f90d259b97c3ad5af78743148dd496f84e6
meghashreemb-tech <meghashree.mb@vidyavikas.edu.in>
2026-06-04 12:21:45 +0530

    Update AI_Insights.py

--- pages/AI_Insights.py
+++ pages/AI_Insights.py
@@ -1,1 +1,53 @@
+import streamlit as st
+import pandas as pd
 
+df = pd.read_csv("data/startup_data.csv")
+
+st.title("🤖 AI Business Insights")
+
+top_valuation = df.loc[
+    df["Valuation (M USD)"].idxmax()
+]
+
+st.success(
+    f"""
+    Highest Valuation Startup:
+    {top_valuation['Startup Name']}
+
+    Valuation:
+    ${top_valuation['Valuation (M USD)']}M
+    """
+)
+
+top_revenue = df.loc[
+    df["Revenue (M USD)"].idxmax()
+]
+
+st.info(
+    f"""
+    Highest Revenue Startup:
+    {top_revenue['Startup Name']}
+    """
+)
+
+industry = (
+    df.groupby("Industry")
+    ["Valuation (M USD)"]
+    .sum()
+    .idxmax()
+)
+
+st.warning(
+    f"Most Valuable Industry: {industry}"
+)
+
+region = (
+    df.groupby("Region")
+    ["Revenue (M USD)"]
+    .sum()
+    .idxmax()
+)
+
+st.success(
+    f"Top Revenue Region: {region}"
+)
